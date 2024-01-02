ca5b984bb579ca53245d443d8284387b19da45e4
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-01-02 22:49:07 +0100

    First component test

--- src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.test.ts
+++ src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.test.ts
@@ -1,20 +1,61 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import {fireEvent, queryByTestId, render} from '@testing-library/vue'
+import ScrumEstimationCard from "../ScrumEstimationCard.vue";
 
-const person = {
-    isActive: true,
-    age: 32,
-}
+describe('ScrumEstimationCard', () => {
+    it('renders', ()=> {
+        const { container } = render(ScrumEstimationCard, {
+            props: {value: "some test value"}
+        })
 
-describe('person', () => {
-    test('person is defined', () => {
-        expect(person).toBeDefined()
+        expect (container).not.toBeNull
     })
 
-    test('is active', () => {
-        expect(person.isActive).toBeTruthy()
+    it('renders the correct value', ()=> {
+        const { container } = render(ScrumEstimationCard, {
+            props: {value: "some test value"}
+        })
+
+        const toggleableCard = queryByTestId(container as HTMLElement, 'scrum-estimation-card') as HTMLDivElement;
+        const textContent = toggleableCard.textContent;
+        expect(textContent).toEqual('some test value')
     })
 
-    test('age limit', () => {
-        expect(person.age).toBeLessThanOrEqual(32)
+    it('selects the card if value and selected value are the same', () => {
+        const { container } = render(ScrumEstimationCard, {
+            props: {
+                value: "some test value",
+                selectedValue: "some test value"
+            }
+        })
+
+        const toggleableCard = queryByTestId(container as HTMLElement, 'scrum-estimation-card') as HTMLDivElement;
+        expect(toggleableCard.classList.contains('awesomescrumestimation-card--selected')).toBeTruthy
+    })
+
+    it('does not select the card if value and selected value are the same', () => {
+        const { container } = render(ScrumEstimationCard, {
+            props: {
+                value: "some test value",
+                selectedValue: "some other value"
+            }
+        })
+
+        const toggleableCard = queryByTestId(container as HTMLElement, 'scrum-estimation-card') as HTMLDivElement;
+        expect(toggleableCard.classList.contains('awesomescrumestimation-card--selected')).toBeFalsy
+    })
+
+    it('emits the selected event on click', async ()=> {
+        const { container, emitted} = render(ScrumEstimationCard, {
+            props: {value: "some test value"}
+        })
+
+        const toggleableCard = queryByTestId(container as HTMLElement, 'scrum-estimation-card') as HTMLDivElement;
+        await fireEvent.click(toggleableCard);
+
+        const selectedEvent = emitted<string[]>('selected');
+        expect(selectedEvent).toBeTruthy;
+        expect(selectedEvent.length).to.equal(1);
+        expect(selectedEvent[0][0]).to.equal("some test value");
     })
 })
