566fdece2051c394d00aa14420e7e7746588a975
Burkhard Blobel <burkhard.blobel@tngtech.com>
2024-01-19 15:37:29 +0100

    Fix function call

--- frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.assertions.ts
+++ frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.assertions.ts
@@ -13,7 +13,7 @@ export const expectVisibleText = (container: HTMLElement, expectedText: string):
 
 export const expectSelectedEventToBeFiredWithValue = (emitted: <T = unknown>(name?: string)=>  T[], valueSelected: String):void => {
     const selectedEvent = emitted<string[]>('selected');
-    expect(selectedEvent).toBeTruthy;
+    expect(selectedEvent).toBeTruthy();
     expect(selectedEvent.length).to.equal(1);
     expect(selectedEvent[0][0]).to.equal(valueSelected);
 }
