fcc67c78e7a8dba29d161bbc428556cffc8d20c3
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-01-28 22:45:57 +0100

    Backend | Fake slow voting

--- backend/src/estimation/estimation.service.ts
+++ backend/src/estimation/estimation.service.ts
@@ -3,65 +3,87 @@ import {
   EstimationResult,
   EstimationVariant,
 } from './estimation.interface';
-import { HttpException, HttpStatus, Logger } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
 
-const estimationResults: Record<EstimationVariant, EstimationResult[]> = {
-  fibonacci: [
-    {
-      name: 'Jon Smith',
-      result: 'cup',
+interface VotingSetting {
+  result: Record<EstimationVariant, string>;
+  maxTimeToVoteS: number;
+}
+
+const votingSettings: Record<string, VotingSetting> = {
+  'John Smith': {
+    result: {
+      fibonacci: '3',
+      shirtSize: 'M',
     },
-    {
-      name: 'Jane Doe',
-      result: '3',
+    maxTimeToVoteS: 20,
+  },
+  'Franz Xaver': {
+    result: {
+      fibonacci: '5',
+      shirtSize: 'L',
     },
-    {
-      name: 'Bob Johnson',
-      result: '5',
+    maxTimeToVoteS: 15,
+  },
+  'Jane Doe': {
+    result: {
+      fibonacci: '8',
+      shirtSize: 'S',
     },
-    {
-      name: 'Alice Brown',
-      result: '3',
-    },
-    {
-      name: 'Tom Wilson',
-      result: undefined,
-    },
-  ],
-  shirtSize: [
-    {
-      name: 'Jon Smith',
-      result: 'L',
-    },
-    {
-      name: 'Jane Doe',
-      result: 'S',
-    },
-    {
-      name: 'Bob Johnson',
-      result: 'M',
-    },
-    {
-      name: 'Alice Brown',
-      result: 'M',
-    },
-    {
-      name: 'Tom Wilson',
-      result: undefined,
-    },
-  ],
+    maxTimeToVoteS: 25,
+  },
 };
 
 export class EstimationService {
   estimationVariant: EstimationVariant = 'fibonacci';
   logger = new Logger('EstimationService');
+  votingStatus = {};
+  startDate = Date.now();
+  constructor() {}
+
+  #resetVotes() {
+    this.startDate = Date.now();
+    for (const name in votingSettings) {
+      this.votingStatus[name] = undefined;
+    }
+  }
+
+  #updateVotingStatus() {
+    const now = Date.now();
+    const deltaTimeInS = (now - this.startDate) / 1000;
+    for (const name in votingSettings) {
+      if (this.votingStatus[name] === undefined) {
+        if (deltaTimeInS > votingSettings[name].maxTimeToVoteS) {
+          this.votingStatus[name] =
+            votingSettings[name].result[this.estimationVariant];
+        } else {
+          if (
+            Math.random() <
+            deltaTimeInS / votingSettings[name].maxTimeToVoteS
+          ) {
+            this.votingStatus[name] =
+              votingSettings[name].result[this.estimationVariant];
+          }
+        }
+      }
+    }
+  }
 
   getResults(): EstimationResult[] {
-    return estimationResults[this.estimationVariant];
+    this.#updateVotingStatus();
+    const result = [];
+    for (const name in this.votingStatus) {
+      result.push({
+        name,
+        result: this.votingStatus[name],
+      });
+    }
+    return result;
   }
 
   startEstimation(options: EstimationOptions) {
     this.estimationVariant = options.variant;
+    this.#resetVotes();
     this.logger.log(
       `Starting estimation with variant ${this.estimationVariant}`,
     );
