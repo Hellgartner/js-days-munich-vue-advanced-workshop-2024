fe8a97100f773513af34bad0efca3cf946254fc3
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-01-28 22:56:13 +0100

    Frontend | Reset votes on voting schema change
    Note: Had to draw the backend calls one level higher for this

--- frontend/src/components/EstimationStatus/EstimationStatus.vue
+++ frontend/src/components/EstimationStatus/EstimationStatus.vue
@@ -4,7 +4,7 @@
 
         <div class="header">Name</div><div class="header">Voting Status</div>
         <div>&nbsp;</div><div>&nbsp;</div>
-        <template v-for="result in results" :key="result.name">
+        <template v-for="result in props.estimationResults" :key="result.name">
           <div>{{ result.name }}</div><div>{{ result.result ? 'voted' : "not yet voted"}}</div>
         </template>
     </div>
@@ -12,38 +12,18 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, onUnmounted, ref} from "vue";
-import type {EstimationVariant} from "@/services/scrumEstimationValuesProvider";
 
+interface EstimationResult {
+  name: string;
+  result?: string;
+}
 interface EstimationStatusProps {
-  currentEstimationVariant: EstimationVariant
+  estimationResults : EstimationResult[]
 }
 
 const props = defineProps<EstimationStatusProps>()
 
-const results = ref([]);
 
-async function fetchVotingResults() {
-  const fetchResult = await fetch("http://localhost:3000/estimation/results")
-  results.value = await fetchResult.json()
-}
-
-let intervalId: number|undefined = undefined;
-
-onMounted( async () => {
-  await fetch("http://localhost:3000/estimation/vote", {method: 'POST', headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({variant: props.currentEstimationVariant})
-  })
-
-  intervalId = setInterval(fetchVotingResults, 1000) as unknown as number
-} )
-
-onUnmounted(() => {
-  clearInterval(intervalId)
-})
 </script>
 
 <style scoped lang="scss">
--- frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
+++ frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="center-content">
     <ScrumEstimation :initial-estimation-variant="currentEstimationVariant" @estimation-variant-changed="setCurrentEstimationVariant"></ScrumEstimation>
-    <EstimationStatus :current-estimation-variant="currentEstimationVariant"></EstimationStatus>
+    <EstimationStatus :estimation-results="results"></EstimationStatus>
   </div>
 </template>
 
 <script setup lang="ts">
   import ScrumEstimation from "@/components/ScrumEstimation/ScrumEstimation.vue";
   import EstimationStatus from "@/components/EstimationStatus/EstimationStatus.vue";
-  import {ref} from "vue";
+  import {onMounted, onUnmounted, ref} from "vue";
   import type {EstimationVariant} from "@/services/scrumEstimationValuesProvider";
-
+  async function startVotingWithCurrentVariant(variant:EstimationVariant) {
+    await fetch("http://localhost:3000/estimation/vote", {
+      method: 'POST', headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({variant})
+    })
+  }
   let currentEstimationVariant = ref<EstimationVariant>('fibonacci');
 
   const setCurrentEstimationVariant = (variant: EstimationVariant) => {
       currentEstimationVariant.value = variant
+      startVotingWithCurrentVariant(variant)
   }
+
+  const results = ref([]);
+
+  async function fetchVotingResults() {
+    const fetchResult = await fetch("http://localhost:3000/estimation/results")
+    results.value = await fetchResult.json()
+  }
+
+  let intervalId: number|undefined = undefined;
+
+  onMounted( async () => {
+    await startVotingWithCurrentVariant(currentEstimationVariant.value);
+    intervalId = setInterval(fetchVotingResults, 1000) as unknown as number
+  } )
+
+  onUnmounted(() => {
+    clearInterval(intervalId)
+  })
 </script>
