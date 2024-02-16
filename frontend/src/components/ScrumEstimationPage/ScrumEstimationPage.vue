1bc2b47848d5981d7bf4e7d819c7d03f778e51f4
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-02-16 22:02:09 +0100

    frontend | move backend code to composable

--- frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
+++ frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
@@ -3,84 +3,40 @@
     Backend not reachable, please reload the page
   </div>
   <div class="center-content">
-    <ScrumEstimation :initial-estimation-variant="currentEstimationVariant"
+    <ScrumEstimation :initial-estimation-variant="initialEstimationVariant"
                      @estimation-variant-changed="setCurrentEstimationVariant"
                      @estimationChanged="updatePlayersResult">
-    >
+      >
     </ScrumEstimation>
-    <EstimationStatus :estimation-results="resultsIncludingPlayerResult" data-testid="estimation-status"></EstimationStatus>
+    <EstimationStatus :estimation-results="resultsIncludingPlayerResult"
+                      data-testid="estimation-status"></EstimationStatus>
   </div>
 </template>
 
 <script setup lang="ts">
-  import ScrumEstimation from "@/components/ScrumEstimation/ScrumEstimation.vue";
-  import EstimationStatus from "@/components/EstimationStatus/EstimationStatus.vue";
-  import {computed, onMounted, onUnmounted, ref} from "vue";
-  import type {EstimationVariant} from "@/services/scrumEstimationValuesProvider";
+import ScrumEstimation from "@/components/ScrumEstimation/ScrumEstimation.vue";
+import EstimationStatus from "@/components/EstimationStatus/EstimationStatus.vue";
+import {computed, ref} from "vue";
+import useVotingBackend, {type EstimationResult} from "@/composables/useVotingBackend";
 
-  interface EstimationResult {
-    name: string;
-    result?: string;
-  }
 
-  const communicationError = ref(false)
-  //to be moved to store + name to be done properly
-  const playerResult = ref<EstimationResult>({
-    name: "player",
-    result: undefined
-  })
+const communicationError = ref(false)
+//to be moved to store + name to be done properly
+const playerResult = ref<EstimationResult>({
+  name: "player",
+  result: undefined
+})
 
-  async function startVotingWithCurrentVariant(variant:EstimationVariant) {
-    try{
-    await fetch("http://localhost:3000/estimation/vote", {
-      method: 'POST', headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({variant})
-    })}
-    catch (e) {
-      console.error(e);
-      communicationError.value = true;
-    }
-  }
-  let currentEstimationVariant = ref<EstimationVariant>('fibonacci');
+const initialEstimationVariant = "fibonacci";
+const {results, setCurrentEstimationVariant} = useVotingBackend(true, initialEstimationVariant)
 
-  const setCurrentEstimationVariant = (variant: EstimationVariant) => {
-      currentEstimationVariant.value = variant
-      startVotingWithCurrentVariant(variant)
-  }
+const resultsIncludingPlayerResult = computed<EstimationResult[]>(() => {
+  return [...results.value, playerResult.value]
+})
 
-  const results = ref<EstimationResult[]>([]);
-
-  async function fetchVotingResults() {
-    try {
-      const fetchResult = await fetch("http://localhost:3000/estimation/results")
-      results.value = await fetchResult.json()
-    } catch (e) {
-      console.error(e)
-      results.value = []
-    }
-  }
-
-  let intervalId: number|undefined = undefined;
-
-  onMounted( async () => {
-    await startVotingWithCurrentVariant(currentEstimationVariant.value);
-    intervalId = setInterval(fetchVotingResults, 1000) as unknown as number
-  } )
-
-  onUnmounted(() => {
-    clearInterval(intervalId)
-  })
-
-  const resultsIncludingPlayerResult = computed<EstimationResult[]>(() => {
-    return [...results.value, playerResult.value]
-  })
-
-  const updatePlayersResult = (result: string|undefined) => {
-    playerResult.value.result = result
-  }
+const updatePlayersResult = (result: string | undefined) => {
+  playerResult.value.result = result
+}
 </script>
 
 <style scoped lang="scss">
