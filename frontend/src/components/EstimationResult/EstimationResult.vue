451bc8f69527ddc9a7eb1331f2ac1ec73d1b70f5
Denise Meinhardt <denise.meinhardt.ext@bestsecret.com>
2024-02-18 18:28:54 +0100

    Frontend | add basic results view

---
+++ frontend/src/components/EstimationResult/EstimationResult.vue
@@ -0,0 +1,69 @@
+<template>
+  <section>
+    <div class="results-table">
+        <div class="header">Name</div><div class="header">Vote</div>
+        <div>&nbsp;</div><div>&nbsp;</div>
+        <template v-for="result in props.estimationResults" :key="result.name">
+          <div>{{ result.name }}</div>
+          <div class="results-table__right-column">{{ result.result }}</div>
+        </template>
+    </div>
+    <div class="button-container">
+      <button @click="goToEstimationPage()" class="button">Reset results</button>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import router from "@/router";
+
+interface EstimationResult {
+  name: string;
+  result?: string;
+}
+interface EstimationStatusProps {
+  estimationResults : EstimationResult[]
+}
+
+const props = defineProps<EstimationStatusProps>()
+
+const goToEstimationPage = () => {
+  router.push({path: "estimation"})
+}
+</script>
+
+<style scoped lang="scss">
+.results-table {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px;
+  border: 3px solid black;
+  max-width: 400px;
+  border-radius: 10px;
+}
+
+.results-table__right-column {
+  justify-items: center;
+}
+
+.header {
+  font-weight: bold;
+}
+
+.button-container {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.button {
+  padding: 10px
+}
+
+.status-icon {
+  height: 1em;
+}
+</style>
--- frontend/src/views/ResultsView.vue
+++ frontend/src/views/ResultsView.vue
@@ -1,4 +1,53 @@
 <template>
-  <h1>Result</h1>
-  ToDo
+  <div class="results-view">
+    <h1 class="results-view__heading">Result</h1>
+    <EstimationResult :estimation-results="results" class="results-view__estimation-status"></EstimationResult>
+  </div>
 </template>
+
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import EstimationResult from "@/components/EstimationResult/EstimationResult.vue";
+
+const results = ref([]);
+
+async function fetchVotingResults() {
+  const fetchResult = await fetch("http://localhost:3000/estimation/results")
+  results.value = await fetchResult.json()
+}
+
+onMounted(async () => {
+  await fetchVotingResults()
+})
+</script>
+
+<style scoped lang="scss">
+.results-view {
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  grid-template-areas:
+    "heading"
+    "status"
+    "reset";
+  min-height: 100vh;
+  min-width: 100vw;
+  grid-row-gap: 20px;
+  font-family: sans-serif;
+}
+
+.results-view__heading {
+  grid-area: heading;
+  padding-bottom: 10px;
+  color: rgb(5, 99, 165);
+  justify-self: center;
+}
+
+.results-view__estimation-status {
+  grid-area: status;
+}
+
+.results-view__reset {
+  grid-area: reset;
+}
+</style>
