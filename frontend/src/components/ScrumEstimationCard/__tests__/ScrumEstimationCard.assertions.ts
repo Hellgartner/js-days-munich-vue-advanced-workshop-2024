0d5d98c357c1610d14105331823ee483586e2490
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-03-07 23:48:43 +0100

    frontend | Exercise 1

--- frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.assertions.ts
+++ frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.assertions.ts
@@ -1,1 +1,22 @@
-//Todo
+import { getByTestId } from '@testing-library/vue'
+import { expect } from 'vitest'
+
+export const getToggleableCard = (container: HTMLElement) => {
+  return getByTestId(container as HTMLElement, 'scrum-estimation-card')
+}
+
+export const expectTextContent = (container: HTMLElement, expectedText: string) => {
+  const scrumEstimationCard = getToggleableCard(container)
+  expect(scrumEstimationCard).toHaveTextContent(expectedText)
+  expect(scrumEstimationCard).toBeVisible()
+}
+
+export const expectCorrectEventToBeEmitted = (
+  emitted: <T = unknown>(name?: string) => T[],
+  valueSelected: String
+) => {
+  const event = emitted<string[]>('selected')
+  expect(event).toHaveLength(1)
+  expect(event[0]).toHaveLength(1)
+  expect(event[0][0]).toEqual(valueSelected)
+}
--- frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.test.ts
+++ frontend/src/components/ScrumEstimationCard/__tests__/ScrumEstimationCard.test.ts
@@ -1,23 +1,72 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { fireEvent, getByTestId, render } from '@testing-library/vue'
+import ScrumEstimationCard from '../ScrumEstimationCard.vue'
+import {
+  expectCorrectEventToBeEmitted,
+  expectTextContent,
+  getToggleableCard
+} from './ScrumEstimationCard.assertions'
+import {
+  expectCardNotToBeSelected,
+  expectCardToBeSelected
+} from '../../ToggleableCard/__tests__/ToggleableCard.assertions'
 
 describe('ScrumEstimationCard', () => {
   it('renders', () => {
-    //ToDo
+    const { container } = render(ScrumEstimationCard, {
+      props: {
+        value: 'test value'
+      }
+    })
+
+    expect(container).not.toBeNull
   })
 
   it('renders the correct value', () => {
-    //ToDo
+    const { container } = render(ScrumEstimationCard, {
+      props: {
+        value: 'test value'
+      }
+    })
+
+    expectTextContent(container as HTMLElement, 'test value')
   })
 
   it('selects the card if value and selected value are the same', () => {
-    //ToDo
+    const { container } = render(ScrumEstimationCard, {
+      props: {
+        value: 'test value',
+        selectedValue: 'test value'
+      }
+    })
+
+    const scrumEstimationCard = getToggleableCard(container as HTMLElement)
+    expectCardToBeSelected(scrumEstimationCard)
   })
 
-  it('does not select the card if value and selected value are the same', () => {
-    //ToDo
+  it('does not select the card if value and selected value are not the same', () => {
+    const { container } = render(ScrumEstimationCard, {
+      props: {
+        value: 'test value',
+        selectedValue: 'some other value'
+      }
+    })
+
+    const scrumEstimationCard = getToggleableCard(container as HTMLElement)
+    expectCardNotToBeSelected(scrumEstimationCard)
   })
 
   it('emits the selected event on click', async () => {
-    //ToDo
+    const { container, emitted } = render(ScrumEstimationCard, {
+      props: {
+        value: 'test value',
+        selectedValue: 'some other value'
+      }
+    })
+
+    const scrumEstimationCard = getToggleableCard(container as HTMLElement)
+    await fireEvent.click(scrumEstimationCard)
+
+    expectCorrectEventToBeEmitted(emitted, 'test value')
   })
 })
