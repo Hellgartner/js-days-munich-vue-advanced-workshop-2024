5009b0310572c40301d24dcb65bcccc655803901
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2024-03-07 22:51:40 +0100

    frontend | shut-up stupid console log warnings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,10 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true'
+  },
   plugins: [
     vue(), svgLoader()
   ],
