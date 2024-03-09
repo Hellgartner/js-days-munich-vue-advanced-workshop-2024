197c8247774bb6d88e1ff16481d6b5e12169d317
Burkhard Blobel <burkhard.blobel@tngtech.com>
2024-03-09 17:16:38 +0100

    frontend | Exercise 3

--- frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
+++ frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
@@ -18,78 +18,24 @@
 <script setup lang="ts">
 import ScrumEstimation from '@/components/ScrumEstimation/ScrumEstimation.vue'
 import EstimationStatus from '@/components/EstimationStatus/EstimationStatus.vue'
-import {computed, onMounted, onUnmounted, ref} from 'vue'
-import type { EstimationResult } from '@/types/EstimationResult'
+import { computed, ref } from 'vue'
+import useVotingBackend, { type EstimationResult } from '@/composables/useVotingBackend'
 import type { EstimationVariant } from '@/services/scrumEstimationValuesProvider'
 
 const initialEstimationVariant: EstimationVariant = 'fibonacci'
 const playerEstimationResult = ref<EstimationResult>({ name: 'Player' })
-const results = ref<EstimationResult[]>([])
-const error = ref<String | undefined>(undefined)
-const loading = ref(true)
-const doPoll = true
+const { results, setCurrentEstimationVariant, error } = useVotingBackend(
+  true,
+  initialEstimationVariant
+)
 
 const resultsIncludingPlayerResult = computed<EstimationResult[]>(() => {
   return [...results.value, playerEstimationResult.value]
 })
 
-async function startVotingWithCurrentVariant(variant: EstimationVariant) {
-  try {
-    await fetch('http://localhost:3000/estimation/vote', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ variant })
-    })
-  } catch (e) {
-    console.error(e)
-    error.value = '' + e
-  }
-}
-const currentEstimationVariant = ref<EstimationVariant>('fibonacci')
-
-const setCurrentEstimationVariant = (newVariant: EstimationVariant): Promise<void> => {
-  currentEstimationVariant.value = newVariant
-  return startVotingWithCurrentVariant(newVariant)
-}
-
 const updatePlayersResult = (result: string | undefined) => {
   playerEstimationResult.value.result = result
 }
-
-async function fetchVotingResults() {
-  try {
-    const fetchResult = await fetch('http://localhost:3000/estimation/results')
-    results.value = await fetchResult.json()
-    loading.value = false
-  } catch (e) {
-    console.error(e)
-    error.value = '' + e
-    results.value = []
-    loading.value = true
-  }
-}
-
-let intervalId: number | undefined = undefined
-
-onMounted(async () => {
-  if (initialEstimationVariant) {
-    await startVotingWithCurrentVariant(currentEstimationVariant.value)
-  }
-  if (doPoll) {
-    intervalId = setInterval(fetchVotingResults, 1000) as unknown as number
-  } else {
-    await fetchVotingResults()
-  }
-})
-
-onUnmounted(() => {
-  if (intervalId) {
-    clearInterval(intervalId)
-  }
-})
 </script>
 
 <style scoped lang="scss">
--- frontend/src/composables/useVotingBackend.ts
+++ frontend/src/composables/useVotingBackend.ts
@@ -1,1 +1,76 @@
-//ToDo
+import type { EstimationVariant } from '@/services/scrumEstimationValuesProvider'
+import { onMounted, onUnmounted, ref } from 'vue'
+
+export interface EstimationResult {
+  name: string
+  result?: string
+}
+
+const useVotingBackend = (doPoll: boolean, initialEstimationVariant?: EstimationVariant) => {
+  const error = ref<String | undefined>(undefined)
+  const loading = ref(true)
+  async function startVotingWithCurrentVariant(variant: EstimationVariant) {
+    try {
+      await fetch('http://localhost:3000/estimation/vote', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ variant })
+      })
+    } catch (e) {
+      console.error(e)
+      error.value = '' + e
+    }
+  }
+  const currentEstimationVariant = ref<EstimationVariant>('fibonacci')
+
+  const setCurrentEstimationVariant = (variant: EstimationVariant): Promise<void> => {
+    currentEstimationVariant.value = variant
+    return startVotingWithCurrentVariant(variant)
+  }
+
+  const results = ref<EstimationResult[]>([])
+
+  async function fetchVotingResults() {
+    try {
+      const fetchResult = await fetch('http://localhost:3000/estimation/results')
+      results.value = await fetchResult.json()
+      loading.value = false
+    } catch (e) {
+      console.error(e)
+      error.value = '' + e
+      results.value = []
+      loading.value = true
+    }
+  }
+
+  let intervalId: number | undefined = undefined
+
+  onMounted(async () => {
+    if (initialEstimationVariant) {
+      await startVotingWithCurrentVariant(currentEstimationVariant.value)
+    }
+    if (doPoll) {
+      intervalId = setInterval(fetchVotingResults, 1000) as unknown as number
+    } else {
+      await fetchVotingResults()
+    }
+  })
+
+  onUnmounted(() => {
+    if (intervalId) {
+      clearInterval(intervalId)
+    }
+  })
+
+  return {
+    results,
+    setCurrentEstimationVariant,
+    error,
+    loading
+  }
+}
+
+export default useVotingBackend
