c178822702794aecf8d896533e1a4325c4cd1da4
Burkhard Blobel <burkhard.blobel@tngtech.com>
2024-03-10 15:18:52 +0100

    frontend | indicate which ToDos belong to exercise 2

--- frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
+++ frontend/src/components/ScrumEstimationPage/ScrumEstimationPage.vue
@@ -18,18 +18,22 @@
 <script setup lang="ts">
 import ScrumEstimation from '@/components/ScrumEstimation/ScrumEstimation.vue'
 import EstimationStatus from '@/components/EstimationStatus/EstimationStatus.vue'
-import { computed, ref } from 'vue'
+import {computed, onMounted, onUnmounted, ref} from 'vue'
 import type { EstimationResult } from '@/types/EstimationResult'
 import type { EstimationVariant } from '@/services/scrumEstimationValuesProvider'
 import dummyEstimationResults from '@/assets/DummyEstimationResults'
 
 const initialEstimationVariant: EstimationVariant = 'fibonacci'
-const playerEstimationResult = ref<EstimationResult>({ name: 'player' })
+const playerEstimationResult = ref<EstimationResult>({ name: 'Player' })
 const results = ref<EstimationResult[]>(dummyEstimationResults)
-const error = ref(false)
+const error = ref<String | false>(false)
 const resultsIncludingPlayerResult = computed<EstimationResult[]>(() => {
   return [...results.value, playerEstimationResult.value]
 })
+
+async function startVotingWithCurrentVariant(variant: EstimationVariant) {
+  // ToDo Exercise 2.1 Part 1/2: Do a backend call to rigger a new voting session
+}
 
 const setCurrentEstimationVariant = (newVariant: EstimationVariant) => {
   playerEstimationResult.value.result = undefined
@@ -38,6 +42,20 @@ const setCurrentEstimationVariant = (newVariant: EstimationVariant) => {
 const updatePlayersResult = (result: string | undefined) => {
   playerEstimationResult.value.result = result
 }
+
+async function fetchVotingResults() {
+  // ToDo Exercise 2.2 Part 1/3: Query voting results from the backend
+}
+
+onMounted(async () => {
+  // ToDo Exercise 2.1 Part 2/2: Start voting new voting
+
+  // ToDo Exercise 2.2 Part 2/3: Start polling voting results
+})
+
+onUnmounted(() => {
+  // ToDo Exercise 2.3 Part 2/3: Do you need to clean-up something? 🤔
+})
 </script>
 
 <style scoped lang="scss">
--- frontend/src/components/ScrumEstimationPage/__tests__/ScrumEstimationPage.test.ts
+++ frontend/src/components/ScrumEstimationPage/__tests__/ScrumEstimationPage.test.ts
@@ -50,11 +50,11 @@ describe('ScrumEstimationPage', () => {
     server?.close()
   })
 
-  it.skip('starts the voting on page load with fibonacci', async () => {
-    //ToDo
+  it('starts the voting on page load with fibonacci', async () => {
+    // ToDo Exercise 2.3
   })
 
-  it.skip('renders the shown voters as soon as the first poll is in', async () => {
-    //ToDo
+  it('renders the shown voters as soon as the first poll is in', async () => {
+    // ToDo Exercise 2.4
   })
 })
